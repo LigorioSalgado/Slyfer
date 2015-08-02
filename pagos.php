7f535b937ae310c31de351304dd1b4a669d937f8
LigorioSalgado <you@example.com>
2015-08-02 11:51:24 -0500

    first full deploy

--- pagos.php
+++ pagos.php
@@ -1,3 +1,14 @@
+<html>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 <?php
 try{
 $nombre=$_POST['nombre'];
@@ -12,8 +23,7 @@ $cs=$_POST['cs'];
 $prop=$_POST['propietario'];
 $con=$_POST['concepto'];
 $total=$_POST['total'];
-echo $mv;
-echo $av;
+
 require_once("simplify/lib/Simplify.php");
    Simplify::$publicKey = 'sbpb_MDhjZmRmNmQtMTJlZC00NmQ1LWI4NDYtZjRhZTg1NjgzZjVi';
    Simplify::$privateKey = 'Q65+PyW5h8jdhFECaykl95C7HNO/PgvqWV5gUDFlGSt5YFFQL0ODSXAOkNtXTToq';
@@ -29,7 +39,12 @@ require_once("simplify/lib/Simplify.php");
             'currency' => 'USD'
     ));
     if ($payment->paymentStatus == 'APPROVED') {
-        echo "Payment approved\n";
+      echo "<center>";
+      echo "<p style='font-size: 50px;'><strong>Slyfer</strong></p>";
+        echo "<i class='fa fa-refresh fa-spin fa-5x'></i>";
+        echo "<p style='font-size: 20px;'><strong>Procesando Tu pago espera unos segundos...</strong></p>";
+          echo "</center>";
+      header('refresh:10; url=paysucefull.php');
     }
 }
 
---
+++ paysucefull.php
@@ -0,0 +1,24 @@
+<html>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+#check {
+    color: #088A08;
+    text-shadow: 1px 1px 1px #ccc;
+    
+}
+
+</style>
+<center>
+<p style='font-size: 50px;'><strong>Slyfer</strong></p>
+  <i class="fa fa-check fa-5x" id="check"></i>
+  <p style='font-size: 20px;'><strong>Pago exitoso en breve seras redirigido..</strong></p>
+  </center>";
+<?php header('refresh:5; url=index.html'); ?>
